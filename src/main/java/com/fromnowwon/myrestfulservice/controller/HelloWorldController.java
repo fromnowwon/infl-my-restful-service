16082617c965a05e64104b84117ce49adba49b3c
Chaewon <fromnowwon@gmail.com>
2024-01-12 14:40:58 +0900

    SECTION_01_06_HelloWorld Bean 추가

--- src/main/java/com/fromnowwon/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/fromnowwon/myrestfulservice/controller/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.fromnowwon.myrestfulservice.controller;
 
+import com.fromnowwon.myrestfulservice.controller.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    // GET
+
+    // URI - /hello-world
+
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
     @GetMapping(path="/hello-world")
     public String helloworld() {
-        return "Hello world!";
+        return "Hello world";
     }
 
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloworldBean() {
+        return new HelloWorldBean("Hello World!!");
+    }
 }
---
+++ src/main/java/com/fromnowwon/myrestfulservice/controller/bean/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.fromnowwon.myrestfulservice.controller.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+//    public HelloWorldBean(String message) {
+//        this.message = message;
+//    }
+}
