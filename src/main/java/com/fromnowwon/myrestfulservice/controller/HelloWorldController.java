96d88dcc78d34bd86800c92c149e79e0d7b20a45
Chaewon <fromnowwon@gmail.com>
2024-01-12 14:18:54 +0900

    SECTION_01_05_HelloWorld Controller 추가

---
+++ src/main/java/com/fromnowwon/myrestfulservice/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.fromnowwon.myrestfulservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path="/hello-world")
+    public String helloworld() {
+        return "Hello world!";
+    }
+
+}
